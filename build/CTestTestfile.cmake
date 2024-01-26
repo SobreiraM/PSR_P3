c03be762420c6440adaa5d407357cdc4e44efcbd
Miguel Sobreira <115593429+SobreiraM@users.noreply.github.com>
2024-01-26 11:03:28 +0000

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ricardo/catkin_ws/src/PSR_P3/robutler_bringup
-# Build directory: /home/ricardo/catkin_ws/src/PSR_P3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
